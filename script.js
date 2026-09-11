12ce82299884f7cb72aaed69676277bb3cff5b65
yiranwang1002 <yiranwang2024@outlook.com>
2026-09-11 07:29:25 -0400

    时钟的基本功能

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-document.getElementById("btn").onclick = function(){
-  alert('JS正常工作！🎉');
+// 更新两个城市时间
+function updateClock() {
+  const now = new Date();
+
+  // 多伦多 UTC-4
+  const torontoOptions = {
+    timeZone: "America/Toronto",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  };
+  const torontoStr = now.toLocaleTimeString("zh-CN", torontoOptions);
+  document.getElementById("toronto-time").innerText = torontoStr;
+
+  // 北京时间 Asia/Shanghai UTC+8
+  const beijingOptions = {
+    timeZone: "Asia/Shanghai",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  };
+  const beijingStr = now.toLocaleTimeString("zh-CN", beijingOptions);
+  document.getElementById("beijing-time").innerText = beijingStr;
 }
+
+// 立刻执行一次
+updateClock();
+// 每1000毫秒(1秒)刷新一次时间
+setInterval(updateClock, 1000);
